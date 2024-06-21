ea3cbe0155c3780fc7b537eeaaa58d0c761dc471
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-06-21 23:19:26 +0300

    квнроь

--- plugins/herramientas-chatgpt.js
+++ plugins/herramientas-chatgpt.js
@@ -4,7 +4,7 @@
 /*       |- [ ⚠ ] - CREDITOS DEL CODIGO - [ ⚠ ] -|      */
 /*     —◉ DESAROLLADO POR OTOSAKA:                       */
 /*     ◉ Otosaka (https://github.com/6otosaka9)          */
-/*     ◉ Número: wa.me/+79011033947                       */
+/*     ◉ Número: wa.me/+79186853241                       */
 /*                                                       */
 /*     —◉ FT:                                            */
 /*     ◉ BrunoSobrino (https://github.com/BrunoSobrino)  */
